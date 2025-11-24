7805cb9c27c22004f56be99382b33d108ddf2540
XdillibX <rsnnrt13@gmail.com>
2025-11-24 13:36:51 +0900

    카드게임 기본 로직 테스트

--- game folder/main.lua
+++ game folder/main.lua
@@ -1,54 +1,45 @@
 function love.load()
-    player = {}
-    player.x = 100
-    player.y = 100
-    player.speed = 200
-    player.isRun = false
-    player.runDuration = 1
-    player.runCooldownTimer = 0
-    player.runTimer = 0
+    background = {}
+    background.image = love.graphics.newImage("/sprites/background01.png")
+
+    Cards = {}
+    function Cards:new(name, imagePath)
+        local instance = setmetatable({}, self)
+        self.__index = self
+
+        instance.name = name
+        instance.image = love.graphics.newImage(imagePath)
+
+        return instance
+    end
+
+    function Cards:info(x, y)
+        love.graphics.print("Card Name: " .. self.name, x, y)
+    end
+
+    RealCard = {}
 end
 
-function love.update(dt)
+function love.keypressed(key)
+    if key == "up" then
+        local newCard = Cards:new("Ace of Spades", "/sprites/pre_card.png")
+        table.insert(RealCard, newCard)
+    end
 
-    if player.isRun then
-        player.runTimer = player.runTimer - dt
-        if player.runTimer <= 0 then
-            player.isRun = false
+    if key == "down" then
+        if #RealCard > 0 then
+            table.remove(RealCard)
         end
-        player.runCooldownTimer = 2
-    end
-
-    if player.runCooldownTimer > 0 then
-        player.runCooldownTimer = player.runCooldownTimer - dt
-    end
-
-    player.speed = player.isRun and 400 or 200
-
-    if love.keyboard.isDown("right") then
-       player.x = player.x + player.speed * dt
-    end
-    if love.keyboard.isDown("left") then
-        player.x = player.x - player.speed * dt
-    end
-    if love.keyboard.isDown("down") then
-        player.y = player.y + player.speed * dt
-    end
-    if love.keyboard.isDown("up") then
-        player.y = player.y - player.speed * dt
     end
 end
 
 function love.draw()
-    love.graphics.print(player.runCooldownTimer > 0 and 
-        string.format("Run Cooldown: %.2f", player.runCooldownTimer) or "Run Ready", 10, 10)
-    love.graphics.setColor(0.2, 0.6, 0.7)
-    love.graphics.circle("fill", player.x, player.y, 20)
-end
+    love.graphics.draw(background.image, 0, 0)
 
-function love.keypressed(key)
-    if key == "space" and player.runCooldownTimer <= 0 then
-        player.isRun = true
-        player.runTimer = player.runDuration
+    for i, card in ipairs(RealCard) do
+        local x = 100 + (i - 1) * 200 
+        local y = 100
+        love.graphics.draw(card.image, x, y)
+        card:info(x, y + 200)
     end
 end
